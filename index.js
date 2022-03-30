300d09b84e67cc50901afb2ecf6b48cf7454c11f
AJ Phoenix <ajphnx_nu@unknown001f0150b4b0.attlocal.net>
2022-03-29 20:35:47 -0400

    finished assignment with bonus:

--- index.js
+++ index.js
@@ -1,44 +1,57 @@
-const newImage = function(imgName,imgSrc, posX, posY){
-    imgName = document.createElement('img')
-    imgName.src = imgSrc
-    imgName.style.position = 'fixed'
-    imgName.style.left = posX
-    imgName.style.bottom = posY
-    document.body.append(imgName)
-    imgName.addEventListener('dblclick', function(){
-        imgName.remove()
+/**************************** 
+ *  +Create Background Tiles
+****************************/
+function tile(tileSrc,posLeft, posBottom, Width, height){
+    for(let h = 0; h < height; h++)
+        {
+            for(let w = 0; w < Width; w++){
+                newImage('bgTile',tileSrc,posLeft + (w*100), posBottom + (h*100))
+            }
+        }
+}
+/**************************** 
+ *  +Create Image Function
+****************************/
+function newImage (imgName,imgSrc, posLeft, posBottom){
+    let element = document.createElement('img')
+    //element.name = imgName
+    element.src = imgSrc
+    element.style.position = 'fixed'
+    element.style.left = posLeft +'px'
+    element.style.bottom = posBottom +'px'
+    document.body.append(element)
+    return element
+}
+/**************************** 
+ *  +Create Item Function
+****************************/
+function newItem (itemName,itemSrc, posLeft, posBottom){
+    let item = newImage(itemName,itemSrc,posLeft,posBottom)
+    item.addEventListener('dblclick', function(){
+        item.remove()
     })
 }
+/**************************** 
+ *  +Generate Background
+****************************/
+let horizon = window.innerHeight * .66
+let skyHeight = window.innerHeight - horizon
+let grassHeight = horizon
 
-newImage('greenCharacter','assets/green-character.gif','100px','100px')
-newImage('pinTree','assets/pine-tree.png','450px','200px')
-newImage('tree','assets/tree.png','200px', '300px')
-newImage('pillar','assets/pillar.png','350px', '100px')
-newImage('crate','assets/crate.png','150px', '200px')
-newImage('well','assets/well.png','500px', '425px')
-newImage('sword','assets/sword.png','500px','405px')
-newImage('shield','assets/sheild.png','165px','185px')
-newImage('staff','assets/staff.png','600px','100px')
-sword.position = 'fixed'
-
-sword.addEventListener('click', function(){
-    sword.remove()
-})
-
-
-/*
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
-
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png '
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
-
-*/
+tile('assets/sky.png', 0, horizon,window.innerWidth/100, skyHeight/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, grassHeight/100)
+/**************************** 
+ *  +New Images
+****************************/
+newImage('greenCharacter','assets/green-character.gif','100','100')
+newImage('pinTree','assets/pine-tree.png','450','200')
+newImage('tree','assets/tree.png','200', '300')
+newImage('pillar','assets/pillar.png','350', '100')
+newImage('crate','assets/crate.png','150', '200')
+newImage('well','assets/well.png','500', '425')
+/**************************** 
+ *  +New items
+****************************/
+newItem('sword','assets/sword.png','500','405')
+newItem('shield','assets/sheild.png','165','185')
+newItem('staff','assets/staff.png','600','100')
